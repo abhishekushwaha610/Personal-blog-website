87bfc70d0802795c8420ca2ea88c0bb793e5dc5f
abhishek kushwahha <abhishekushwaha610@users.noreply.github.com>
2020-04-24 13:34:34 +0530

    added productuion

---
+++ frontend/build/precache-manifest.f37db812d4910c154924d261f5ebc7f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2508c9b6a904f1cce00f36b41ff541",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3437684ad1dd3cffcd20",
+    "url": "/static/css/2.4a4875ba.chunk.css"
+  },
+  {
+    "revision": "6298b029a3d0df7e7894",
+    "url": "/static/css/main.c53a0b0b.chunk.css"
+  },
+  {
+    "revision": "3437684ad1dd3cffcd20",
+    "url": "/static/js/2.fa80b08b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fa80b08b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6298b029a3d0df7e7894",
+    "url": "/static/js/main.f40e08b0.chunk.js"
+  },
+  {
+    "revision": "cd5cc0b019c8434d78aa",
+    "url": "/static/js/runtime-main.95a77404.js"
+  }
+]);
